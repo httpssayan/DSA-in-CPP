9470c362f645b131d1200f58a51a7e429dc0b913
httpssayan <sayan5564jana@gmail.com>
2026-01-09 10:34:59 +0530

    Add files via upload

--- Arrays/Find_the_no_that_appears_once_with_others_given_twice.cpp
+++ Arrays/Find_the_no_that_appears_once_with_others_given_twice.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int no_that_appears_once(int arr[],int n){
-    int xor=0;
+    int xorr=0;
     for(int i=0;i<n;i++){
-        xor=xor^arr[i];
+        xorr=xorr^arr[i];
     }
-    return xor;
+    return xorr;
 }
 int main(){
     int arr[]={2,3,5,4,5,3,4};

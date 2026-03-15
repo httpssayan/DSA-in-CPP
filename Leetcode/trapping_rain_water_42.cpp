e7d23de488bf394d082421e3929364978c47eaf0
Sayan Jana <sayan5564jana@gmail.com>
2026-03-15 21:16:49 +0530

    Implement trapping rainwater algorithm and update main function for testing

---
+++ Leetcode/trapping_rain_water_42.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+
+        int left=0;
+        int right=n-1;
+
+        int maxleft=0;
+        int maxright=0;
+
+        int water=0;
+
+        while(left<=right){
+            if(height[left]<=height[right]){
+                if(maxleft<=height[left]){
+                    maxleft=height[left];
+                }
+                else{
+                    water+=maxleft - height[left];
+                }
+                left++;
+            }
+            else{
+                if(maxright<=height[right]){
+                    maxright=height[right];
+                }
+                else{
+                    water+=maxright - height[right];
+                }
+                right--;
+            }
+            }
+        return water;
+    }
+};

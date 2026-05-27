d9690a7f3801e9cc80f90e990a6340e1954485cb
Sayan Jana <sayan5564jana@gmail.com>
2026-05-27 11:30:02 +0530

    Add implementation for asteroid collision detection using stack

---
+++ Stacks and Queues/asteroid_collision.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> asteroids_collision(vector<int>& asteroids){
+    stack<int> s;
+    for(int x:asteroids){
+        bool destroyed = false;
+        while(!s.empty() && s.top()>0 && x<0){
+            if(s.top() < abs(x)){
+                s.pop();
+            }
+            else{
+                destroyed = true;
+                break;
+            }
+        }
+        if(!destroyed){
+            s.push(x);
+        }
+    }
+    vector<int> result;
+    while(!s.empty()){
+        result.push_back(s.top());
+        s.pop();
+    }
+    reverse(result.begin(), result.end());
+    return result;
+}
+
+int main(){
+    vector<int> asteroids = {5, 10, -5};
+    vector<int> result = asteroids_collision(asteroids);
+    for(int x:result){
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
